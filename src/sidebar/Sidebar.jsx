936c79f7d0c9ffab491e606bf8ff85c9df966559
Hamed Bostan <hamed.bostan2@gmail.com>
2024-06-05 20:09:59 +0330

    vertical line is removed from drawer

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -9,8 +9,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { useUIContext } from '../context/ui';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import AddToDriveOutlinedIcon from '@mui/icons-material/AddToDriveOutlined';
@@ -81,7 +79,7 @@ export const Sidebar = () => {
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
 
-      <Drawer variant="permanent" open={open}>
+      <Drawer variant="permanent" open={open} PaperProps={{style: {border: 'none'}}}  >
 
         <DrawerHeader />
 
