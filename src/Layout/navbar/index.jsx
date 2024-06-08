82a354f468bc7ec786deb6a2852f8b99a23838d1
Hamed Bostan <hamed.bostan2@gmail.com>
2024-06-08 17:17:16 +0330

    hover effect on navbar is edited

--- src/Layout/navbar/index.jsx
+++ src/Layout/navbar/index.jsx
@@ -1,7 +1,10 @@
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import MenuIcon from '@mui/icons-material/Menu';
-import { IconButton, Typography, styled } from '@mui/material';
+import { IconButton, Avatar, Badge, Box, InputBase, styled, Icon } from '@mui/material';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
+import MicOutlinedIcon from '@mui/icons-material/MicOutlined';
 import { useUIContext } from '../../context/ui';
 
 const AppBar = styled(MuiAppBar, {
@@ -20,6 +23,16 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
+
+const Search = styled("div")(({ theme }) => ({
+  width: "90%",
+  borderRadius: 20,
+  height: 40,
+  border: "1px solid gray",
+  display: 'flex',
+  alignItems: "center"
+}))
+
 export const Navbar = () => {
   const { open, setOpen } = useUIContext()
 
@@ -29,18 +42,41 @@ export const Navbar = () => {
 
   return (
     <AppBar position="fixed" open={open} elevation={0}>
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawer}
-          edge="start"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap component="div">
-          Mini variant drawer
-        </Typography>
+      <Toolbar sx={{ bgcolor: '#fff', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawer}
+            edge="start"
+            sx={{ color: '#0f0f0f' }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src='./images/logo.jpg' alt='Youtube logo' width={"115px"} />
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center', width: "40%" }} gap={2}>
+          <Search >
+            <InputBase placeholder="Search" sx={{ ml: 2, width: "95%" }} />
+          </Search>
+          <Box sx={{
+            bgcolor: 'rgba(0,0,0,0.05)', borderRadius: "50%", width: 38, height: 38, display: 'flex', justifyContent: 'center', alignItems: 'center', ":hover": {
+              bgcolor: 'rgba(0,0,0,0.1)'
+            }
+          }}>
+            <MicOutlinedIcon sx={{ color: "#0f0f0f" }} />
+          </Box>
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center', }} gap={3}>
+          <VideocamOutlinedIcon sx={{ color: "#0f0f0f" }} />
+          <Badge badgeContent={"9+"} color='error'>
+            <NotificationsNoneOutlinedIcon sx={{ color: "#0f0f0f" }} />
+          </Badge>
+          <Avatar src='./images/profileImages/caleb-curry.jpg' sx={{ width: 38, height: 38 }} />
+        </Box>
+
       </Toolbar>
     </AppBar>
   )
