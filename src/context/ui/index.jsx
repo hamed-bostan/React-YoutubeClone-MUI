292dfd89d18cb1330e61d65ee8d7ef52b9762342
Hamed Bostan <hamed.bostan2@gmail.com>
2024-06-15 16:22:42 +0330

    navbar tempOpen and sidebar tempOpen is deleted

--- src/context/ui/index.jsx
+++ src/context/ui/index.jsx
@@ -4,11 +4,9 @@ export const UIContext = createContext();
 export const useUIContext = () => useContext(UIContext)
 
 export const UIProvider = ({ children }) => {
-    // const [open, setOpen] = useState(false);
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     const value = {
-        // open, setOpen,
         isDrawerOpen, setIsDrawerOpen
     }
     return <UIContext.Provider value={value}>{children}</UIContext.Provider>
