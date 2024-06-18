8c99fd8ecd57b76085df760c13521c47ce1690f7
Hamed Bostan <hamed.bostan2@gmail.com>
2024-06-18 08:37:35 +0330

    data for videos is added

--- src/components/CategoryPills.jsx
+++ src/components/CategoryPills.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
-import { categories } from "../data";
+import { categories } from "../data/categories"; 
 import { Box, Button, Typography } from "@mui/material";
 import { DrawerHeader } from "../Layout/sidebar";
 import { capitalizeFirstLetter } from "../utility/Utilities";
