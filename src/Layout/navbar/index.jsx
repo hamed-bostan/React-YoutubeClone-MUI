0d89146e964dc53d8c85726ff6faf0f987f7ff2e
Hamed Bostan <hamed.bostan2@gmail.com>
2024-06-27 09:21:19 +0330

    navbarList styles is cleaned

--- src/Layout/navbar/index.jsx
+++ src/Layout/navbar/index.jsx
@@ -1,21 +1,9 @@
-import MenuIcon from '@mui/icons-material/Menu';
 import { useUIContext } from '../../context/ui';
-import { useState } from 'react';
-import { AutoComplete } from '../../components/autoComplete';
-import { Tooltip } from '@mui/material';
-
-
-import { MenuANDLogoContainer, MenuIconButton, CameraIcon, CameraIconButton, MicIcon, AutoCompleteContainer, IconsContainer, LogoIconButton, MicIconButton, NotificationIcon, NotificationIconButton, SearchContainer, StyledAvatar, YoutubeIcon, YoutubeText, StackContainer, StyledToolbar, StyledAppBar, StyledBadge }
-  from './styles';
-
+import { StackContainer, StyledToolbar, StyledAppBar } from './styles';
+import { NavbarList } from '../../components/navbarList';
 
 export const Navbar = () => {
-  const { isDrawerOpen, setIsDrawerOpen } = useUIContext()
-  const [isFocus, setIsFocus] = useState(false)
-
-  const handleDrawer = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-  };
+  const { isDrawerOpen } = useUIContext()
 
   return (
     <>
@@ -23,47 +11,7 @@ export const Navbar = () => {
         <StyledToolbar disableGutters>
           <StackContainer>
 
-            <MenuANDLogoContainer>
-              <MenuIconButton aria-label="open drawer" disableTouchRipple onClick={handleDrawer}>
-                <MenuIcon />
-              </MenuIconButton>
-              <LogoIconButton disableRipple>
-                <YoutubeIcon />
-                <YoutubeText>YouTube</YoutubeText>
-              </LogoIconButton>
-            </MenuANDLogoContainer>
-
-
-            <SearchContainer>
-              <Tooltip title='Search with your voice'>
-                <MicIconButton disableRipple>
-                  <MicIcon />
-                </MicIconButton>
-              </Tooltip>
-
-              <AutoCompleteContainer isFocus={isFocus}>
-                <AutoComplete isFocus={isFocus} setIsFocus={setIsFocus} />
-              </AutoCompleteContainer>
-            </SearchContainer>
-
-
-            <IconsContainer>
-              <Tooltip title='Create'>
-                <CameraIconButton disableRipple>
-                  <CameraIcon />
-                </CameraIconButton>
-              </Tooltip>
-
-              <Tooltip title='Notifications'>
-                <NotificationIconButton disableRipple>
-                  <StyledBadge badgeContent={"9+"} color='error' >
-                    <NotificationIcon />
-                  </StyledBadge>
-                </NotificationIconButton>
-              </Tooltip>
-
-              <StyledAvatar src='./images/profileImages/caleb-curry.jpg' />
-            </IconsContainer>
+            <NavbarList />
 
           </StackContainer>
         </StyledToolbar>
